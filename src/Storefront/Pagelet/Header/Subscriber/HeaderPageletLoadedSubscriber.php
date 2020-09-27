b6881b2ef320d3c0cb4c66cbd370b7df12f4c341
ufcyg <mellowski@gmail.com>
2020-09-27 23:29:05 +0200

    first commit

---
+++ src/Storefront/Pagelet/Header/Subscriber/HeaderPageletLoadedSubscriber.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Synlab\PlatformConversionHeaderPro\Storefront\Pagelet\Header\Subscriber;
+
+use Shopware\Storefront\Pagelet\Header\HeaderPageletLoadedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Shopware\Core\Framework\Struct\ArrayEntity;
+
+class HeaderPageletLoadedSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            HeaderPageletLoadedEvent::class => 'onHeaderPageletLoaded'
+        ];
+    }
+
+    public function onHeaderPageletLoaded(HeaderPageletLoadedEvent $event): void
+    {
+        $systemConfig = $this->systemConfigService->get('PlatformConversionHeaderPro.config');
+        $page = $event->getPagelet();
+
+        $page->addExtension('CHP', new ArrayEntity($systemConfig));
+
+        /*echo '<pre>';
+        print_r($systemConfig);
+        die();*/
+    }
+
+    /**
+     * @var SystemConfigService
+     * 
+     * $backgroundColor = $this->systemConfigService->get('SynlabPlatformConversionHeaderPro.config.backgroundColor');
+     * 
+     */
+    private $systemConfigService;
+
+    public function __construct(SystemConfigService $systemConfigService)
+    {
+        $this->systemConfigService = $systemConfigService;
+    }
+}
